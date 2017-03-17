06a54f0819dd48052bbd5a77a1e92696d3634e59
Lucien Hubert <phillipehubert@gmail.com>
2017-03-17 14:35:38 +0100

    New route

--- src/ProjectBundle/Controller/DefaultController.php
+++ src/ProjectBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
@@ -16,7 +16,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/playlists")
+     * @Route("/playlists", name="playlists_list")
      */
     public function playlistsAction()
     {
