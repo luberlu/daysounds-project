22f23863362ce94a0def98c71fc1cd0179e35cf3
Lucien Hubert <phillipehubert@gmail.com>
2017-03-20 20:58:26 +0100

    New Entity Players

--- src/ProjectBundle/Entity/Genre.php
+++ src/ProjectBundle/Entity/Genre.php
@@ -4,6 +4,10 @@ namespace ProjectBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * Genre
  *
