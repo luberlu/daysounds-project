77d665029120a0cf773a2502c38fc3eec65c59f9
Lucien Hubert <phillipehubert@gmail.com>
2017-03-27 23:30:59 +0200

    Add to this playlist action work now

--- src/ProjectBundle/Controller/SoundController.php
+++ src/ProjectBundle/Controller/SoundController.php
@@ -10,21 +10,22 @@ use ProjectBundle\Model\AddSoundType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class SoundController extends DefaultController {
 
     /**
-     * @Route("/{$playlist_slug}/add/{$sound_id}", name="add_sound_to_this_playlist", requirements={"sound_id" = "\d+"})
-     * @param $playlist_slug
+     * @Route("/playlist/{playlist_id}/add/{sound_id}", name="add_sound_to_this_playlist", requirements={"sound_id" = "\d+"})
+     * @param $playlist_id
      * @param $sound_id
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function addSoundToPlaylist($playlist_slug, $sound_id){
+    public function addSoundPlaylist($playlist_id, $sound_id){
 
         if($this->getUser()) {
             $user = $this->getUser();
             $em = $this->getDoctrine()->getManager();
 
-            $playlist = $this->getDoctrine()->getRepository("ProjectBundle:Playlist")->findOneBySlug($playlist_slug);
+            $playlist = $this->getDoctrine()->getRepository("ProjectBundle:Playlist")->findOneById($playlist_id);
             $sound = $this->getDoctrine()->getRepository("ProjectBundle:Sound")->findOneById($sound_id);
             $playlist->addSound($sound);
             $em->persist($playlist);
