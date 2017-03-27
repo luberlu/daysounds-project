51064a61cd9aa4e9cb5bf08bf83c891173ecb09f
Lucien Hubert <phillipehubert@gmail.com>
2017-03-27 10:39:58 +0100

    Add follow action added on Profil

--- src/ProjectUserBundle/Controller/DefaultController.php
+++ src/ProjectUserBundle/Controller/DefaultController.php
@@ -14,4 +14,32 @@ class DefaultController extends Controller
     {
         return $this->render('ProjectUserBundle:Default:index.html.twig');
     }
+
+
+    /**
+     * @Route("/follow/{slug_username}", name="add_follow")
+     * @param $slug_username
+     */
+    public function addFollowByTo($slug_username){
+
+        if($this->getUser()){
+
+            $user = $this->getDoctrine()->getRepository("ProjectUserBundle:User")->findOneBySlug($slug_username);
+            $em = $this->getDoctrine()->getManager();
+
+            $user->addRelationUser($this->getUser());
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('user-profil',
+                array('slug_username'=> $this->getUser()->getSlug()
+                )
+            ));
+
+        }
+
+
+
+
+    }
 }
